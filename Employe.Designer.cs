51dc2d7d8ae7777e7eda42ba225f683a05fb9db8
LOUBNATANASSA <loubnatanassa553@gmail.com>
2024-12-13 19:10:19 +0100

    Employe CRUD

---
+++ Employe.Designer.cs
@@ -0,0 +1,435 @@
+﻿namespace HOTEL_MANAGMENT
+{
+    partial class Employe
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges25 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges26 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges27 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges28 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges29 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges30 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges31 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges32 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges33 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges34 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges35 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges36 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Nom = new Label();
+            Prenom = new Label();
+            Adresse = new Label();
+            Telephone = new Label();
+            DateRejoin = new Label();
+            loginE = new Label();
+            MotDePass = new Label();
+            NomBox = new TextBox();
+            CINBox = new TextBox();
+            TelephoneBox = new TextBox();
+            MotDePassBox = new TextBox();
+            IdentifiantBox = new TextBox();
+            AdresseBox = new TextBox();
+            PrenomBox = new TextBox();
+            AddEmp = new Guna.UI2.WinForms.Guna2Button();
+            DeleteEmp = new Guna.UI2.WinForms.Guna2Button();
+            UpdateEmp = new Guna.UI2.WinForms.Guna2Button();
+            ReadEmp = new Guna.UI2.WinForms.Guna2Button();
+            DateRejoinBox = new Guna.UI2.WinForms.Guna2DateTimePicker();
+            listViewEmp = new ListView();
+            columnHeader9 = new ColumnHeader();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            columnHeader6 = new ColumnHeader();
+            columnHeader7 = new ColumnHeader();
+            columnHeader8 = new ColumnHeader();
+            CIN = new Label();
+            consulterButton = new Guna.UI2.WinForms.Guna2Button();
+            getid = new Label();
+            SuspendLayout();
+            // 
+            // Nom
+            // 
+            Nom.AutoSize = true;
+            Nom.Location = new Point(12, 86);
+            Nom.Name = "Nom";
+            Nom.Size = new Size(52, 25);
+            Nom.TabIndex = 1;
+            Nom.Text = "Nom";
+            // 
+            // Prenom
+            // 
+            Prenom.AutoSize = true;
+            Prenom.Location = new Point(525, 92);
+            Prenom.Name = "Prenom";
+            Prenom.Size = new Size(74, 25);
+            Prenom.TabIndex = 2;
+            Prenom.Text = "Prenom";
+            // 
+            // Adresse
+            // 
+            Adresse.AutoSize = true;
+            Adresse.Location = new Point(972, 92);
+            Adresse.Name = "Adresse";
+            Adresse.Size = new Size(75, 25);
+            Adresse.TabIndex = 3;
+            Adresse.Text = "Adresse";
+            // 
+            // Telephone
+            // 
+            Telephone.AutoSize = true;
+            Telephone.Location = new Point(12, 165);
+            Telephone.Name = "Telephone";
+            Telephone.Size = new Size(92, 25);
+            Telephone.TabIndex = 5;
+            Telephone.Text = "Telephone";
+            // 
+            // DateRejoin
+            // 
+            DateRejoin.AutoSize = true;
+            DateRejoin.Location = new Point(494, 171);
+            DateRejoin.Name = "DateRejoin";
+            DateRejoin.Size = new Size(127, 25);
+            DateRejoin.TabIndex = 6;
+            DateRejoin.Text = "Date de Rejoin";
+            // 
+            // loginE
+            // 
+            loginE.AutoSize = true;
+            loginE.Location = new Point(1421, 177);
+            loginE.Name = "loginE";
+            loginE.Size = new Size(92, 25);
+            loginE.TabIndex = 7;
+            loginE.Text = "Identifiant";
+            // 
+            // MotDePass
+            // 
+            MotDePass.AutoSize = true;
+            MotDePass.Location = new Point(961, 171);
+            MotDePass.Name = "MotDePass";
+            MotDePass.Size = new Size(120, 25);
+            MotDePass.TabIndex = 8;
+            MotDePass.Text = "Mot De Passe";
+            // 
+            // NomBox
+            // 
+            NomBox.Location = new Point(147, 86);
+            NomBox.Name = "NomBox";
+            NomBox.Size = new Size(303, 31);
+            NomBox.TabIndex = 9;
+            // 
+            // CINBox
+            // 
+            CINBox.Location = new Point(1478, 89);
+            CINBox.Name = "CINBox";
+            CINBox.Size = new Size(303, 31);
+            CINBox.TabIndex = 11;
+            // 
+            // TelephoneBox
+            // 
+            TelephoneBox.Location = new Point(147, 165);
+            TelephoneBox.Name = "TelephoneBox";
+            TelephoneBox.Size = new Size(303, 31);
+            TelephoneBox.TabIndex = 12;
+            // 
+            // MotDePassBox
+            // 
+            MotDePassBox.Location = new Point(1087, 171);
+            MotDePassBox.Name = "MotDePassBox";
+            MotDePassBox.Size = new Size(303, 31);
+            MotDePassBox.TabIndex = 14;
+            // 
+            // IdentifiantBox
+            // 
+            IdentifiantBox.Location = new Point(1519, 171);
+            IdentifiantBox.Name = "IdentifiantBox";
+            IdentifiantBox.Size = new Size(303, 31);
+            IdentifiantBox.TabIndex = 15;
+            // 
+            // AdresseBox
+            // 
+            AdresseBox.Location = new Point(1069, 89);
+            AdresseBox.Name = "AdresseBox";
+            AdresseBox.Size = new Size(303, 31);
+            AdresseBox.TabIndex = 16;
+            // 
+            // PrenomBox
+            // 
+            PrenomBox.Location = new Point(627, 86);
+            PrenomBox.Name = "PrenomBox";
+            PrenomBox.Size = new Size(303, 31);
+            PrenomBox.TabIndex = 17;
+            // 
+            // AddEmp
+            // 
+            AddEmp.CustomizableEdges = customizableEdges25;
+            AddEmp.DisabledState.BorderColor = Color.DarkGray;
+            AddEmp.DisabledState.CustomBorderColor = Color.DarkGray;
+            AddEmp.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            AddEmp.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            AddEmp.Font = new Font("Segoe UI", 9F);
+            AddEmp.ForeColor = Color.White;
+            AddEmp.Location = new Point(114, 724);
+            AddEmp.Name = "AddEmp";
+            AddEmp.ShadowDecoration.CustomizableEdges = customizableEdges26;
+            AddEmp.Size = new Size(246, 55);
+            AddEmp.TabIndex = 18;
+            AddEmp.Text = "Ajouter Employe";
+            AddEmp.Click += AddEmp_Click;
+            // 
+            // DeleteEmp
+            // 
+            DeleteEmp.CustomizableEdges = customizableEdges27;
+            DeleteEmp.DisabledState.BorderColor = Color.DarkGray;
+            DeleteEmp.DisabledState.CustomBorderColor = Color.DarkGray;
+            DeleteEmp.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            DeleteEmp.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            DeleteEmp.Font = new Font("Segoe UI", 9F);
+            DeleteEmp.ForeColor = Color.White;
+            DeleteEmp.Location = new Point(1268, 724);
+            DeleteEmp.Name = "DeleteEmp";
+            DeleteEmp.ShadowDecoration.CustomizableEdges = customizableEdges28;
+            DeleteEmp.Size = new Size(245, 57);
+            DeleteEmp.TabIndex = 19;
+            DeleteEmp.Text = "Supprimer Employe";
+            DeleteEmp.Click += DeleteEmp_Click;
+            // 
+            // UpdateEmp
+            // 
+            UpdateEmp.CustomizableEdges = customizableEdges29;
+            UpdateEmp.DisabledState.BorderColor = Color.DarkGray;
+            UpdateEmp.DisabledState.CustomBorderColor = Color.DarkGray;
+            UpdateEmp.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            UpdateEmp.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            UpdateEmp.Font = new Font("Segoe UI", 9F);
+            UpdateEmp.ForeColor = Color.White;
+            UpdateEmp.Location = new Point(564, 726);
+            UpdateEmp.Name = "UpdateEmp";
+            UpdateEmp.ShadowDecoration.CustomizableEdges = customizableEdges30;
+            UpdateEmp.Size = new Size(257, 55);
+            UpdateEmp.TabIndex = 20;
+            UpdateEmp.Text = "Modifier Employe";
+            UpdateEmp.Visible = false;
+            UpdateEmp.Click += UpdateEmp_Click;
+            // 
+            // ReadEmp
+            // 
+            ReadEmp.CustomizableEdges = customizableEdges31;
+            ReadEmp.DisabledState.BorderColor = Color.DarkGray;
+            ReadEmp.DisabledState.CustomBorderColor = Color.DarkGray;
+            ReadEmp.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            ReadEmp.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            ReadEmp.Font = new Font("Segoe UI", 9F);
+            ReadEmp.ForeColor = Color.White;
+            ReadEmp.Location = new Point(1607, 724);
+            ReadEmp.Name = "ReadEmp";
+            ReadEmp.ShadowDecoration.CustomizableEdges = customizableEdges32;
+            ReadEmp.Size = new Size(215, 57);
+            ReadEmp.TabIndex = 21;
+            ReadEmp.Text = "Afficher Employe";
+            ReadEmp.Click += ReadEmp_Click;
+            // 
+            // DateRejoinBox
+            // 
+            DateRejoinBox.Checked = true;
+            DateRejoinBox.CustomizableEdges = customizableEdges33;
+            DateRejoinBox.FillColor = Color.FromArgb(94, 148, 255);
+            DateRejoinBox.Font = new Font("Segoe UI", 9F);
+            DateRejoinBox.Format = DateTimePickerFormat.Long;
+            DateRejoinBox.Location = new Point(627, 165);
+            DateRejoinBox.MaxDate = new DateTime(9998, 12, 31, 0, 0, 0, 0);
+            DateRejoinBox.MinDate = new DateTime(1753, 1, 1, 0, 0, 0, 0);
+            DateRejoinBox.Name = "DateRejoinBox";
+            DateRejoinBox.ShadowDecoration.CustomizableEdges = customizableEdges34;
+            DateRejoinBox.Size = new Size(303, 45);
+            DateRejoinBox.TabIndex = 22;
+            DateRejoinBox.Value = new DateTime(2024, 12, 12, 22, 29, 45, 104);
+            // 
+            // listViewEmp
+            // 
+            listViewEmp.Columns.AddRange(new ColumnHeader[] { columnHeader9, columnHeader1, columnHeader2, columnHeader3, columnHeader4, columnHeader5, columnHeader6, columnHeader7, columnHeader8 });
+            listViewEmp.Location = new Point(21, 281);
+            listViewEmp.Name = "listViewEmp";
+            listViewEmp.Size = new Size(1891, 355);
+            listViewEmp.TabIndex = 23;
+            listViewEmp.UseCompatibleStateImageBehavior = false;
+            listViewEmp.View = View.Details;
+            // 
+            // columnHeader9
+            // 
+            columnHeader9.Text = "id";
+            columnHeader9.Width = 70;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Nom";
+            columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Prenom";
+            columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Adresse";
+            columnHeader3.Width = 200;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "CIN";
+            columnHeader4.Width = 150;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "Telephone";
+            columnHeader5.Width = 200;
+            // 
+            // columnHeader6
+            // 
+            columnHeader6.Text = "Date de Rejoin";
+            columnHeader6.Width = 250;
+            // 
+            // columnHeader7
+            // 
+            columnHeader7.Text = "Mot De Pass";
+            columnHeader7.Width = 150;
+            // 
+            // columnHeader8
+            // 
+            columnHeader8.Text = "Identifiant";
+            columnHeader8.Width = 150;
+            // 
+            // CIN
+            // 
+            CIN.AutoSize = true;
+            CIN.Location = new Point(1421, 95);
+            CIN.Name = "CIN";
+            CIN.Size = new Size(41, 25);
+            CIN.TabIndex = 24;
+            CIN.Text = "CIN";
+            // 
+            // consulterButton
+            // 
+            consulterButton.CustomizableEdges = customizableEdges35;
+            consulterButton.DisabledState.BorderColor = Color.DarkGray;
+            consulterButton.DisabledState.CustomBorderColor = Color.DarkGray;
+            consulterButton.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            consulterButton.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            consulterButton.Font = new Font("Segoe UI", 9F);
+            consulterButton.ForeColor = Color.White;
+            consulterButton.Location = new Point(564, 726);
+            consulterButton.Name = "consulterButton";
+            consulterButton.ShadowDecoration.CustomizableEdges = customizableEdges36;
+            consulterButton.Size = new Size(257, 55);
+            consulterButton.TabIndex = 25;
+            consulterButton.Text = "Cosulter Employe";
+            consulterButton.Click += consulterButton_Click;
+            // 
+            // getid
+            // 
+            getid.AutoSize = true;
+            getid.Location = new Point(73, 241);
+            getid.Name = "getid";
+            getid.Size = new Size(27, 25);
+            getid.TabIndex = 26;
+            getid.Text = "id";
+            getid.Visible = false;
+            // 
+            // Employe
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1924, 835);
+            Controls.Add(getid);
+            Controls.Add(consulterButton);
+            Controls.Add(CIN);
+            Controls.Add(listViewEmp);
+            Controls.Add(DateRejoinBox);
+            Controls.Add(ReadEmp);
+            Controls.Add(UpdateEmp);
+            Controls.Add(DeleteEmp);
+            Controls.Add(AddEmp);
+            Controls.Add(PrenomBox);
+            Controls.Add(AdresseBox);
+            Controls.Add(IdentifiantBox);
+            Controls.Add(MotDePassBox);
+            Controls.Add(TelephoneBox);
+            Controls.Add(CINBox);
+            Controls.Add(NomBox);
+            Controls.Add(MotDePass);
+            Controls.Add(loginE);
+            Controls.Add(DateRejoin);
+            Controls.Add(Telephone);
+            Controls.Add(Adresse);
+            Controls.Add(Prenom);
+            Controls.Add(Nom);
+            Margin = new Padding(4);
+            Name = "Employe";
+            Text = "Form1";
+            WindowState = FormWindowState.Maximized;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label Nom;
+        private Label Prenom;
+        private Label Adresse;
+        private Label Telephone;
+        private Label DateRejoin;
+        private Label loginE;
+        private Label MotDePass;
+        private TextBox NomBox;
+        private TextBox CINBox;
+        private TextBox TelephoneBox;
+        private TextBox MotDePassBox;
+        private TextBox IdentifiantBox;
+        private TextBox AdresseBox;
+        private TextBox PrenomBox;
+        private Guna.UI2.WinForms.Guna2Button AddEmp;
+        private Guna.UI2.WinForms.Guna2Button DeleteEmp;
+        private Guna.UI2.WinForms.Guna2Button UpdateEmp;
+        private Guna.UI2.WinForms.Guna2Button ReadEmp;
+        private Guna.UI2.WinForms.Guna2DateTimePicker DateRejoinBox;
+        private ListView listViewEmp;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private ColumnHeader columnHeader6;
+        private ColumnHeader columnHeader7;
+        private ColumnHeader columnHeader8;
+        private ColumnHeader columnHeader9;
+        private Label CIN;
+        private Guna.UI2.WinForms.Guna2Button consulterButton;
+        private Label getid;
+    }
+}

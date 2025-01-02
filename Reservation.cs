84bedfe3e5532326a0ac3d9c2121ac35a423c196
umaimaes <essaoudoumaima@gmail.com>
2025-01-02 21:22:06 +0100

    affichage spa & cars services

--- Reservation.cs
+++ Reservation.cs
@@ -45,12 +45,55 @@ namespace HOTEL_MANAGMENT
 
         private void Carcheckbox_CheckedChanged(object sender, EventArgs e)
         {
-            
+            if (Carcheckbox.Checked)
+            {
+                SearchCarLocationBtn.Visible = true;
+                labelMsg.Visible = true;
+                label3.Visible = true;
+                DateDebutCarLocation.Visible = true;
+                label7.Visible = true;
+                DateFinCarLocation.Visible = true;
+                label6.Visible = true;
+                labelNom.Visible = true;
+                label5.Visible = true;
+                labelMarque.Visible = true;
+                label4.Visible = true;
+                labelCouleur.Visible = true;
+                ListViewCar.Visible = true;
+                label9.Visible = true;
+
+            }
+            else
+            {
+                SearchCarLocationBtn.Visible = false;
+                labelMsg.Visible = false;
+                label3.Visible = false;
+                DateDebutCarLocation.Visible = false;
+                label7.Visible = false;
+                DateFinCarLocation.Visible = false;
+                label6.Visible = false;
+                labelNom.Visible = false;
+                label5.Visible = false;
+                labelMarque.Visible = false;
+                label4.Visible = false;
+                labelCouleur.Visible = false;
+                ListViewCar.Visible = false;
+                label9.Visible = false;
+            }
         }
 
         private void Spacheckbox_CheckedChanged(object sender, EventArgs e)
         {
-            
+            if (Spacheckbox.Checked)
+            {
+                Nbr_Seances_Label.Visible = true;
+                Nbr_Seances_Box.Visible = true;
+            }
+            else
+            {
+                Nbr_Seances_Label.Visible = false;
+                Nbr_Seances_Box.Visible = false;
+            }
         }
 
         private void SearchCarLocationBtn_Click_1(object sender, EventArgs e)
